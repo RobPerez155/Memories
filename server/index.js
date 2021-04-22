9046c4290c5f098da73623d860e333ad8b117664
RobPerez155 <robert.perez.psirho@gmail.com>
2021-04-22 16:26:39 -0400

    .env files - Completed

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser' // Parse incoming request bodies in a middleware before the handlers
 import mongoose from 'mongoose'
 import cors from 'cors' // Allows AJAX requests to skip the Same-origin policy and access resources from remote hosts
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express() // Here we initialize the app first
+dotenv.config();
 
 // Setting up bodyParser so we can properly send a request  
 app.use(bodyParser.json({limit: '30mb', extended: true}))
@@ -14,11 +15,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.qgo54.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 // Here we will connect to our Mongoose database
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => 
     console.log(`Server running on port ${PORT}`))
   ).catch((error) => console.log(error.message))
